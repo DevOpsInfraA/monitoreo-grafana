9437932a51bb0d4c2838c4e8b0ede24838e4b67e
Elias <EliasAgustinBordon0715@gmail.com>
2024-06-26 15:26:48 -0300

    update check_nc

--- textfile-collector/check_nc.sh
+++ textfile-collector/check_nc.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 DIR=/opt/accusys/monitoreo-grafana/textfile-collector/check_nc.prom
-nc -z -w 2 192.168.172.57 1433 > /dev/null #Configurar ip y puerto correspondiente a su BD
+IP=192.168.172.57
+PORT=1433
+
+nc -z -w 2 $IP $PORT > /dev/null #Configurar ip y puerto correspondiente a su BD
 
 if [ $? -eq 0 ]; then
-  echo "check_nc{target=\"192.168.172.71\"} 1" > $DIR
+  echo "check_nc{target=\"$IP\"} 1" > $DIR
 else
-  echo "check_nc{target=\"192.168.172.71\"} 0" > $DIR
+  echo "check_nc{target=\"$IP\"} 0" > $DIR
 fi
